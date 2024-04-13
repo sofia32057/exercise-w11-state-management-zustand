7868675da91bef83ce42427be0415ef8b02b51f1
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-13 18:02:28 +0200

    done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import { useTodoStore } from "../store/useTodoStore";
+
 export const TodoList = () => {
-  return <div>Create a form to add a todo to your global state here 📝</div>
-}
+  const { addTodo, todos, deleteTodo } = useTodoStore();
+  const [task, setTask] = useState("");
+
+  const handleChange = event => {
+    setTask(event.target.value);
+    console.log(task);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    addTodo(task);
+    setTask("");
+  };
+
+  const handleRemove = (event, i) => {
+    event.preventDefault();
+    deleteTodo(i);
+    console.log("event", event);
+  };
+
+  return (
+    <main>
+      {/* the form */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task-input"
+          id="task-input"
+          value={task}
+          onChange={handleChange}
+        />
+        <button>Add task</button>
+      </form>
+
+      {/* The list */}
+      <ul>
+        {todos.map((task, i) => (
+          <li key={`${task}${i}`}>
+            {task}{" "}
+            <button
+              onClick={event => {
+                handleRemove(event, i);
+              }}>
+              remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
--- src/store/useTodoStore.jsx
+++ src/store/useTodoStore.jsx
@@ -1,14 +1,18 @@
-import create from "zustand"
+import { create } from "zustand";
 
-export const useTodoStore = create((set) => ({
-  todos: [],
-  addTodo: () => {
-    set()
+export const useTodoStore = create(set => ({
+  todos: ["Finish this excersise", "start project", "eat chicken"],
+  addTodo: task => {
+    set(state => ({ todos: [...state.todos, task] }));
   },
-  deleteTodo: () => {
-    set()
+  deleteTodo: index => {
+    set(state => {
+      const newTodos = [...state.todos];
+      newTodos.splice(index, 1);
+      return { todos: newTodos };
+    });
   },
   completeTodo: () => {
-    set()
+    set();
   },
-}))
+}));
